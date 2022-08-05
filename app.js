c7b9a34374df932bd7d69a9c6c4eb739eb0efaea
Letale-vc <letalerv@gmail.com>
2022-08-05 20:41:09 +0300

    small change

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const app = express()
 // настройка корс
 const whitelist = [
   'http://localhost:3000',
-  'http://127.0.0.1:3000',
   'http://31.131.100.39:3000',
   'https://poe-flip.helpless.keenetic.link',
   'http://poe-flip.helpless.keenetic.link',
